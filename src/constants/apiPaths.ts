8501f1957f8057bd1ef210b024c9ca1378c3f75f
Igor Deriabin <igor@deriab.in>
2024-03-26 18:06:37 +0300

    Connect product-service API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
+  order: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
+  import: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
+  bff: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
+  cart: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
 };
 
 export default API_PATHS;
