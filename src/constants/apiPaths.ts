1daf36c1368f60d3faee8435f9cffd405ae4a588
Igor Deriabin <igor@deriab.in>
2024-04-27 19:23:20 +0300

    Enable CSV file upload

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
   order: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
-  import: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
+  import: "https://ntnzjvmgu3.execute-api.eu-central-1.amazonaws.com",
   bff: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
   cart: "https://3b1jao89b1.execute-api.eu-central-1.amazonaws.com",
 };
